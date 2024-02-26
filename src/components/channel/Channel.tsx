c7ed36ee8866f17b1c13fc2e42285eb095985b85
CarinWood <carin.wood.85@gmail.com>
2024-02-26 12:24:41 +0100

    update

--- src/components/channel/Channel.tsx
+++ src/components/channel/Channel.tsx
@@ -2,15 +2,17 @@ import { FC } from "react";
 import "./channel.css";
 
 type channelProps = {
-  src: string,
-  name: string
-}
+  src: string;
+  name: string;
+  startRadio: (name: string) => void;
+};
 
-const Channel:FC<channelProps> = ({src, name}) => {
+const Channel: FC<channelProps> = ({ src, name, startRadio }) => {
   return (
-  <div>
-    <img src={src} alt={name} className="channel-image" /> 
-  </div>)
+    <div onClick={() => startRadio(name)}>
+      <img src={src} alt={name} className="channel-image" />
+    </div>
+  );
 };
 
 export default Channel;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ export const Home = () => {
   const [p1, setP1] = useState();
   const [p2, setP2] = useState();
   const [p3, setP3] = useState();
+  const [radioNow, setRadioNow] = useState("");
 
   async function getPrograms() {
     const response = await fetch(
@@ -21,17 +22,38 @@ export const Home = () => {
     getPrograms();
   }, []);
 
-  console.log(p1);
+  function startRadio(name: string) {
+    if (name === "P1") {
+      setRadioNow(p1.liveaudio.url);
+    } else if (name === "P2") {
+      setRadioNow(p2.liveaudio.url);
+    } else {
+      setRadioNow(p3.liveaudio.url);
+    }
+  }
+
+  console.log(radioNow);
 
   return (
     <div>
       <section className="live">
         <p className="heading">Lyssna direkt</p>
         <article className="channels">
-          {p1 && <Channel src={p1.image} name={p1.name} />}
-          {p2 && <Channel src={p2.image} name={p2.name} />}
-          {p3 && <Channel src={p3.image} name={p3.name} />}
+          {p1 && (
+            <Channel src={p1.image} name={p1.name} startRadio={startRadio} />
+          )}
+          {p2 && (
+            <Channel src={p2.image} name={p2.name} startRadio={startRadio} />
+          )}
+          {p3 && (
+            <Channel src={p3.image} name={p3.name} startRadio={startRadio} />
+          )}
         </article>
+      </section>
+      <section className="audio-section">
+        <audio controls key={radioNow} autoPlay>
+          <source src={radioNow} type="audio/mpeg" />
+        </audio>
       </section>
     </div>
   );
