63016fccab4fca55e1274bfd8162c36a0f1d57e5
CarinWood <carin.wood.85@gmail.com>
2024-02-28 13:56:35 +0100

    update

--- src/components/chanelItem/ChannelItem.tsx
+++ src/components/chanelItem/ChannelItem.tsx
@@ -4,14 +4,18 @@ import { FC } from "react";
 import { IoIosPlay } from "react-icons/io";
 
 const ChanelItem: FC<ChannelProps> = ({ name, img }) => {
+  const startAudio = () => {
+    console.log(name);
+  };
+
   return (
     <div className="channel-item">
       <img src={img} alt={name} className="_image" />
       <div className="bottom-part">
         <p className="channel-item-name">{name}</p>
-        <p className="listen-now">
+        <p className="listen-now" onClick={startAudio}>
           Lyssna nu
-          <IoIosPlay  className="play-arrow"/>
+          <IoIosPlay className="play-arrow" />
         </p>
       </div>
     </div>
