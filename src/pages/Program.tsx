d534823ef3d363b16f3e52ba0580bb0b04a14e40
CarinWood <carin.wood.85@gmail.com>
2024-03-04 10:56:31 +0100

    added showCategory function

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -6,7 +6,7 @@ const Program: FC<programProps> = ({ p1 }) => {
   async function getPrograms(id: number) {
     try {
       const response = await fetch(
-        `http://api.sr.se/api/v2/programs/index?channelid=${id}&&format=json&&page=1&&size=10`
+        `http://api.sr.se/api/v2/programs/index?programcategoryid=${id}&&format=json&&page=1&&size=10`
       );
       const data = await response.json();
       console.log(data);
@@ -15,11 +15,29 @@ const Program: FC<programProps> = ({ p1 }) => {
     }
   }
 
-  getPrograms(p1);
+
+  const showCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategoryId = parseInt(event.target.value)
+    getPrograms(selectedCategoryId)
+  }
 
   return (
     <div className="program-contianer">
-      <div></div>
+      <div className="categories">
+        <p className="category-p">Välj kategori:</p>
+        <select onChange={showCategory}>
+          <option value={2}>Barn 3 - 8 år</option>
+          <option value={132}>Barn 9 - 13 å</option>
+          <option value={82}>Dokumentär</option>
+          <option value={134}>Drama</option>
+          <option value={135}>Ekonomi</option>
+          <option value={133}>Humor</option>
+          <option value={3}>Kultur/Nöje</option>
+          <option value={14}>Livsstil</option>
+          <option value={4}>Livsåskådning</option>
+          <option value={5}>Musik</option>
+        </select>
+      </div>
     </div>
   );
 };
