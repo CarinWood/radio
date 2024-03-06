eba805131ad9095bcc4fb9167964c592d9d744aa
CarinWood <carin.wood.85@gmail.com>
2024-03-06 11:28:00 +0100

    logo update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import snail from "../../assets/images/NicePng_snail-png_1031942.png";
 
 const Header = () => {
   return (
     <header>
+      <div className="logo">
+        <p className="logo-text">Snigelradio</p>
+        <img src={snail} alt="snail" className="snail-img" />
+      </div>
       <ul className="list">
-        <Link to="/"><li>Hem</li></Link>
-        <Link to="/kanaler"><li>Kanaler</li></Link>
-        <Link to="/program"><li>Program</li></Link>
+        <Link to="/">
+          <li>Hem</li>
+        </Link>
+        <Link to="/kanaler">
+          <li>Kanaler</li>
+        </Link>
+        <Link to="/program">
+          <li>Program</li>
+        </Link>
       </ul>
     </header>
   );
